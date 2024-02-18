f5f438539e5959625493ea053829c6abcd025708
Sufiyan6201 <154262186+Sufiyan6201@users.noreply.github.com>
2024-02-18 23:44:49 +0530

    hii

--- asset/js/main.js
+++ asset/js/main.js
@@ -70,8 +70,8 @@ var products = [
 var productHtml = "";
 products.forEach((products) => {
   productHtml += ` <div class=" col-md-3 text-center ">
-                <div class="card">
-                <div class="cards mt-2">
+                <div class="card  mt-4 mb-4">
+                <div class="cards">
                     <div class="overflow-hidden">
                         <img src="${products.img}" class="pr object-fit-cover" height="300px"alt="">
                     </div>
